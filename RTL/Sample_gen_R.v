1bba2a24d55829bcb985dec7d1f18dc510993220
ShivanandaMateti <shivamateti60@gmail.com>
2026-06-22 20:07:54 +0530

    changed the samplegen

--- RTL/Sample_gen_R.v
+++ RTL/Sample_gen_R.v
@@ -1,30 +1,28 @@
-// Sampling_rate_receiver = ( baud_rate_T * 8 ) = (9600*8) 76800 samples/sec;
+module Sample_gen (
+                    input r_clk,
+                    input reset,
+                    input restart,
+                    output Sample_tick
+                  );
 
-module Sample_gen_R (
-    input   clk,
-    input   reset,
-    output  Sample_tick_R
-);
-integer count = 1;
-reg Sample_tick_R_reg;
-
-initial Sample_tick_R_reg <= 1'b0;
-always @(posedge clk) begin
-    if(reset)
-    begin
-       count <= 1;
-       Sample_tick_R_reg  <= 1'b1;
-    end
-    else if(count==5) begin     // count = 5 since // time period of clock = 2.604167us  // one sample tick of receiver occurs for every 0.013020833ms
-       count <= 1;
-       Sample_tick_R_reg  <= 1'b1;
+reg [2:0] count;
+reg Sample_tick_reg;
+always@(posedge r_clk,posedge reset)begin
+    if(reset || restart)begin
+        Sample_tick_reg <= 0;
+        count           <= 1;
     end
     else begin
-        count <= count + 1;
-        Sample_tick_R_reg <= 1'b0;
-    end        
+        count       <= count + 3'd1;
+        if(count == 3'd4)begin
+            Sample_tick_reg <= 1;
+            count           <= 1;
+        end
+        else
+            Sample_tick_reg <= 0;
+    end
 end
+assign Sample_tick  = Sample_tick_reg; 
 
-assign Sample_tick_R = Sample_tick_R_reg;
 
 endmodule
